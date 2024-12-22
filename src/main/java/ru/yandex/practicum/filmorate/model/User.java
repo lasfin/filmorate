1b572552fcd91529628c111038850bb85b4970cd
e.kopitsa <e.kopitsa@semrush.com>
2024-12-22 13:13:18 +0100

    User model with constraints

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Must be a valid email address")
     private String email;
+    @NotEmpty(message = "Login cannot be empty")
+    @Pattern(regexp = "^\\S+$",
+            message = "Login cannot contain whitespace characters")
     private String login;
     private String name;
-    private Date birthday;
+    @PastOrPresent(message = "Birthday should be in the past or present")
+    private LocalDate birthday;
 }
