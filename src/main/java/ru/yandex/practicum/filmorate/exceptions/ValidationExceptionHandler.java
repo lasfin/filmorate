aae6b7771e731eed154164120f9ca5527c5bf6c4
e.kopitsa <e.kopitsa@semrush.com>
2024-12-23 18:33:06 +0100

    add logging for ValidationExceptionHandler

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,13 +14,21 @@ import java.util.Map;
 @RestControllerAdvice
 public class ValidationExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ValidationExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+            log.warn("Validation error on field: '{}', message: '{}'", field, message);
+        });
+        if (!errors.isEmpty()) {
+            log.warn("Validation failed with {} errors", errors.size());
+        }
         return errors;
     }
 }
