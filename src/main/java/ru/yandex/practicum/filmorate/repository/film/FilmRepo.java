a65359e26808c0d02d2590f96a8404d6e81d35f3
e.kopitsa <e.kopitsa@semrush.com>
2025-01-12 14:23:36 +0100

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/repository/film/FilmRepo.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/film/FilmRepo.java
@@ -6,11 +6,18 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmRepo {
+
     ResponseEntity<Film> createFilm(Film film);
+
     ResponseEntity<Film> updateFilm(Film film);
+
     ResponseEntity<Film> deleteFilm(Film film);
+
     ResponseEntity<List<Film>> getFilms();
+
     ResponseEntity<Film> addLike(Long filmId, Long userId);
+
     ResponseEntity<Film> removeLike(Long filmId, Long userId);
+
     ResponseEntity<List<Film>> getPopularFilms(Integer count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/repository/user/UserRepo.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/user/UserRepo.java
@@ -6,14 +6,22 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserRepo {
+
     ResponseEntity<User> createUser(User user);
+
     ResponseEntity<User> updateUser(User user);
+
     ResponseEntity<User> deleteUser(User user);
+
     ResponseEntity<User> getUser(Long userId);
+
     ResponseEntity<List<User>> getUsers();
 
     ResponseEntity<User> addFriend(Long userId, Long friendId);
+
     ResponseEntity<User> removeFriend(Long userId, Long friendId);
+
     ResponseEntity<List<User>> getFriends(Long userId);
+
     ResponseEntity<List<User>> getCommonFriends(Long userId, Long friendId);
 }
